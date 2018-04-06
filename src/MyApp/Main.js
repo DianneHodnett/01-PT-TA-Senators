6c4d46a7e8e30a7ce9aec3a5f69817ed130fd9e8
Edxael <zerphyr.r@gmail.com>
2018-04-05 22:38:21 -0600

    update code

--- src/MyApp/Main.js
+++ src/MyApp/Main.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import SenData from './senators'
 import Template from './Template'
+import './99-style.css'
 
 
-
-//this is the scope of the class
 export default class Main extends React.Component{
 
     state = { showpol: false, senators: [] }
@@ -19,29 +18,21 @@ export default class Main extends React.Component{
 
 
         const FilterDemocrats = () => {
-            const DemSen = SenData.filter((politician) => {
-                return politician.party === "Democrat"
-            })
-            console.log(DemSen)  //this is step Two of the assignment. Filter the Democrat Senators.
+            const DemSen = SenData.filter((politician) => { return politician.party === "Democrat" })
             this.setState({ senators: DemSen , showpol: true })
         }
 
 
 
         const FilterUtahSenators = () => {
-            const UtahSen = SenData.filter( (politician) => { return politician.state === "UT"  }) })
+            const UtahSen = SenData.filter( (politician) => { return politician.state === "UT"  } )
             this.setState({ senators: UtahSen , showpol: true })
-            console.log(UtahSen)
         }
 
 
         const FilterUtahSenior = () => {
             const Senior = SenData.filter( (politician) => { return politician.description === "Senior Senator from Utah"  })
-            console.log(Senior[0].person.firstname)  //this is step Four of the assignment. Filter the Utah Senior Senators.
-
             this.setState({ senators: Senior , showpol: true })
-            
-            console.log(Senior)
         }
 
 
@@ -50,39 +41,26 @@ export default class Main extends React.Component{
             console.log(this.state.senators)
             return this.state.senators.map((politician) => { return <Template key={politician.phone} info={politician} />  })
         }
-
-        console.log(this.state.showpol)
+        
 
         return(
             <div>
                 <h1>Senators Homework</h1>
-                <hr/>
 
-                <button onClick={ FilterRepublicans} >Republicans</button>
-
-                <br></br>
-
-                <button onClick={ FilterDemocrats} >Democrats</button>
-                <br></br>
-
-                <button onClick={ FilterUtahSenators } >Utah Senators</button>
-                <br></br>
-
-                <button onClick={ FilterUtahSenior } >Utah Senior Senator</button>
-                <br></br>
+                <div>
+                    <button className="MyButton" onClick={ FilterRepublicans} >Republicans</button>
+                    <button className="MyButton" onClick={ FilterDemocrats} >Democrats</button>
+                    <button className="MyButton" onClick={ FilterUtahSenators } >Utah Senators</button>
+                    <button className="MyButton" onClick={ FilterUtahSenior } >Utah Senior Senator</button>
+                </div>    
+             
 
                 <hr/>
-
-              
                 
-                { this.state.showpol ? React.createElement(showData) : <div>No Data</div> }
+                { this.state.showpol ? React.createElement(showData) : <div>Click a button</div> }
 
             </div>
         )
     }
 }
-
-// { this.state.showdem ? React.createElement(FilterDemocrats) : <div>No Data</div> }
-
-
 
--- src/MyApp/Template.js
+++ src/MyApp/Template.js
@@ -1,19 +1,16 @@
 import React from 'react'
+import './99-style.css'
 
 export default class Template extends React.Component{
     render(){
         return(
-            <div>
+            <div className="TempCont" >
                 <h3>{ this.props.info.description}</h3> 
                 <div><strong>Name: </strong>{ this.props.info.person.name }</div>
                 <div><strong>Party: </strong>{ this.props.info.party }</div>
                 <div><strong>State: </strong>{ this.props.info.state }</div>
-              
-                <hr/>
             </div>
         )
     }
 }
-
-
 
